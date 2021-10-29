fba3e6f469d1e43713f626d96ab89e0eaea6eeb8
galfleissig <fleissigal@gmail.com>
2021-10-28 20:58:08 -0400

    Implementation of Q6.5 - Math and Logic Puzzles

---
+++ Chapter6-5.py
@@ -0,0 +1,14 @@
+"""
+
+Answer to Question 6.5
+
+We fill the 3 quart jug all the way, then pour it into the 5 quart jug.
+Then we fill the 3 quart jug again all the way, and pour it to the 5 quart
+jug until the 5 quart jug gets full. We will have poured 2 quarts, and we will
+have had 1 quart remaining in the 3 quart jug.
+Then we dump the water from the 5 quart jug and fill it with the 1 quart that we
+have in the 3 quart jug.
+We then fill the 3 quart jug all the way and pour it into the 5 quart jug,
+which gives us 1 + 3 = 4 quarts of water.
+
+"""
