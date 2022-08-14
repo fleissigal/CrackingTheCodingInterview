51702db491d6f45970750bcc9ef4a567adeb689a
fleissigal <fleissigal@gmail.com>
2022-08-13 20:33:36 -0400

    Add solution for question 7.3

---
+++ Chapter7-3.py
@@ -0,0 +1,48 @@
+"""
+Answer to Question 7.3
+"""
+
+from collections import deque
+from enum import Enum
+
+class Song:
+    duration = ""
+    artist = ""
+    title = ""
+
+    def playSong(self):
+        return
+
+class Juke:
+    archive = [] #array of songs
+    playlist = deque() #queue of songs
+    payments = [] #array og payments
+
+    def getPayments(self):
+        return
+    def getNextSong(self):
+        return
+    def searchSong(self):
+        return
+
+class Payment:
+    amount = 0
+
+class User:
+    name = ""
+    payments = [] #array of payments
+
+    def shuffle(self):
+        return
+    def addSong(self):
+        return
+    def makePayment(self):
+        return
+    def deleteSong(self):
+        return
+
+class Display:
+    playing = "" #current song playing
+
+    def showSong(self):
+        return
